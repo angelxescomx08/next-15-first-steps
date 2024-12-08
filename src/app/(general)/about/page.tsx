31b89be9f045178f4c06680ee7c164292e173db5
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2024-12-08 12:23:44 -0600

    navbar

---
+++ src/app/(general)/about/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+ title: 'SEO Title',
+ description: 'SEO Title',
+ keywords: ['SEO', 'Title'],
+};
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export const Navbar = async () => {
+  return (
+    <nav className="bg-blue-800 flex bg-opacity-30 p-2 m-2 rounded">
+      <span>Home</span>
+      <div className="flex flex-1"></div>
+      <Link className="mr-2" href={'/about'}>About</Link>
+      <Link className="mr-2" href={'/pricing'}>Pricing</Link>
+      <Link className="mr-2" href={'/contact'}>Contact</Link>
+    </nav>
+  )
+}
